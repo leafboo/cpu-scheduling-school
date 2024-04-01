deea3e4ac53046f3176cdd4ca5cbbdd76ddd2885
Jan Romel <janjancaranguian@gmail.com>
2024-04-01 11:07:45 +0800

    Move function to top and make display_output_table function

--- cpuScheduling.py
+++ cpuScheduling.py
@@ -1,23 +1,3 @@
-print('\n--------CPU Scheduling--------')
-print('First Come First Served (FCFS)')
-print('Shortest Job First (SJF)')
-print('Shortest Remaining Time (SRT)')
-print('Round Robin (RR)')
-print('Priority Scheduling (PS)')
-print('------------------------------')
-
-user_choice = input('Please enter your desired cpu scheduling: ')
-
-cpu_scheduling_mappings = {
-  'FCFS': 'First Come First Served',
-  'SJF': 'Shortest Job First',
-  'SRT': 'Shortest Remaining Time',
-  'RR': 'Round Robin',
-  'PR': 'Priority Scheduling'
-}
-
-chosen_cpu_scheduling = cpu_scheduling_mappings.get(user_choice)
-
 
 def get_user_input():
   try:
@@ -46,25 +26,59 @@ def get_user_input():
     print('Input is not a number. Try again.\n')
 
 
+def display_output_table(chosen_cpu_scheduling):
+  processes, arrival_times, burst_times = get_user_input()
+
+  print(f'\n{chosen_cpu_scheduling}')
+  print('-------------------------------')
+  print('P        AT        BT        CT')
+  for i in range(1, processes + 1):
+    print(f'P{i}        {arrival_times[i - 1]}        {burst_times[i - 1]}')
+
+
 def compute_fcfs(): 
   processes, arrival_times, burst_times = get_user_input()
 
   sorted_processes = sorted(zip(arrival_times, burst_times), key=lambda x: x[0])
+  arrival_times_copy, burst_times_copy = zip(*sorted_processes)
 
   completion_time = [0] * processes
+  completion_time[0] = arrival_times_copy[0] + burst_times_copy[0]
+  for i in range(1, processes):
+    completion_time[i] = completion_time[i - 1] + burst_times_copy[i]
+
   turnaround_time = [0] * processes
   waiting_time = [0] * processes
 
-  # sort arrival time
-  sorted_arrival_times = sorted(arrival_times)
 
 
 
+
+
+print('\n--------CPU Scheduling--------')
+print('First Come First Served (FCFS)')
+print('Shortest Job First (SJF)')
+print('Shortest Remaining Time (SRT)')
+print('Round Robin (RR)')
+print('Priority Scheduling (PS)')
+print('------------------------------')
+
+user_choice = input('Please enter your desired cpu scheduling: ')
+
+cpu_scheduling_mappings = {
+  'FCFS': 'First Come First Served',
+  'SJF': 'Shortest Job First',
+  'SRT': 'Shortest Remaining Time',
+  'RR': 'Round Robin',
+  'PR': 'Priority Scheduling'
+}
+
+chosen_cpu_scheduling = cpu_scheduling_mappings.get(user_choice)
 
 
 if (chosen_cpu_scheduling):
   print(f'You have chosen: {chosen_cpu_scheduling}\n')
-  compute_fcfs()
+  display_output_table(chosen_cpu_scheduling)
 else:
   print('Invalid input. Try again.')
 
