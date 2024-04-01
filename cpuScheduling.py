99c72887c3fd2cf5dd24e3b0103e203fbd41117c
Jan Romel <janjancaranguian@gmail.com>
2024-04-01 22:12:53 +0800

    Finish compute_fcfs function

--- cpuScheduling.py
+++ cpuScheduling.py
@@ -11,13 +11,13 @@ def get_user_input():
     arrival_times = []
     print('\nEnter the arrival times: ')
     for i in range(1, processes + 1):
-      arrival_time = int(input((f'P{i}: ')))
+      arrival_time = int(input((': ')))
       arrival_times.append(arrival_time)
     
     burst_times = []
     print('\nEnter the burst times: ')
     for i in range(1, processes + 1):
-      burst_time = int(input(f'P{i}: '))
+      burst_time = int(input(': '))
       burst_times.append(burst_time)
 
     return processes, arrival_times, burst_times
@@ -27,28 +27,35 @@ def get_user_input():
 
 
 def display_output_table(chosen_cpu_scheduling):
-  processes, arrival_times, burst_times = get_user_input()
+  processes, arrival_times, burst_times, completion_time, turnaround_time, waiting_time = compute_fcfs()
 
   print(f'\n{chosen_cpu_scheduling}')
   print('-------------------------------')
-  print('P        AT        BT        CT')
+  print('P        AT        BT        CT        TAT        WT')
   for i in range(1, processes + 1):
-    print(f'P{i}        {arrival_times[i - 1]}        {burst_times[i - 1]}')
+    print(f'P{i}        {arrival_times[i - 1]}        {burst_times[i - 1]}         {completion_time[i - 1]}        {turnaround_time[i - 1]}        {waiting_time[i - 1]}')
 
 
 def compute_fcfs(): 
   processes, arrival_times, burst_times = get_user_input()
 
   sorted_processes = sorted(zip(arrival_times, burst_times), key=lambda x: x[0])
-  arrival_times_copy, burst_times_copy = zip(*sorted_processes)
+  arrival_times, burst_times = zip(*sorted_processes)
 
   completion_time = [0] * processes
-  completion_time[0] = arrival_times_copy[0] + burst_times_copy[0]
+  completion_time[0] = arrival_times[0] + burst_times[0]
   for i in range(1, processes):
-    completion_time[i] = completion_time[i - 1] + burst_times_copy[i]
+    completion_time[i] = completion_time[i - 1] + burst_times[i]
 
   turnaround_time = [0] * processes
+  for i in range(processes):
+    turnaround_time[i] = completion_time[i] - arrival_times[i]
+
   waiting_time = [0] * processes
+  for i in range(processes):
+    waiting_time[i] = turnaround_time[i] - burst_times[i]
+
+  return processes, arrival_times, burst_times, completion_time, turnaround_time, waiting_time
 
 
 
